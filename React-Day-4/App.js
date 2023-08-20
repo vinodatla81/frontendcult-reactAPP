8495ec793088c4228a3fde6557c7a497cdf36bc2
vinod <vinodatla81@gmail.com>
2023-08-20 16:20:01 +0530

    props & multicomponents

--- React-Day-4/App.js
+++ React-Day-4/App.js
@@ -1,57 +1,109 @@
 import React from "react";
 import ReactDOM from "react-dom";
- 
-// React Create Element
-const h1tag = React.createElement("div", { id : "parent"}, "some text");
 
+/**
+ * Hearder component
+ * -logo
+ * -Menu item
+ * Body component
+ *  -search Bar
+ *  -RestoComponent
+ *     -Image
+ *     -Resto Name
+ *     -Rating
+ *     -Delivery Time
+ * Footer Component
+ *   - Copyrights
+ *   - Links
+ * Properties for components
+ */
 
-// JSX - 5%
-// Babel - 
-let name = "Vinod";
-let mobile = "8331834181"
-const jsXdivision = <div id="myid" className="my-div"><p>my name is  { name } and mobile numer is {mobile}</p>
-<p id="name">rregrjhhhji</p> JSX
-</div>;
-
-// React Components
-
-// Everything in react components are 2 ways:
-// 1) class based components (old way)
-// 2) Functional/methods based components (99.9 % we use this)
-// ............Components.............
-
-const userDetails = {
-    username : "vinod",
-    mobile : 8331834181
-}
-const ButtonCOM = () => (
-    <input type="text"/>
-);
-const HeaderComponent = () => (
-    <div class ="header"><h2>my Header</h2><ButtonCOM/></div>
-);
-
-const ContentComponent = () =>  { 
-    return(<div class ="content">
-    <h2>The user name: {userDetails.username}</h2>
-    </div>)    
+const HeaderComponent = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg?w=100" alt="log"></img>
+            </div>
+            <div className="navigation">
+                <ul>
+                    <li>Home</li>
+                    <li>About US</li>
+                    <li>Services</li>
+                    <li>Top Offers</li>
+                    <li>my card</li>
+                </ul>
+            </div>
+        </div>
+    )
 };
+
+const RestoComponent = (props) => {
+    console.log(props);
+    return (
+        <div className="resto">
+            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/3632e911860bd3a90f9558fc81372b73" alt=""/>
+            <h2>{props.resName}</h2>
+            <p>{props.resRating } </p>
+            <h4>{props.resDeliverytime}</h4>
+            <h4>{props.resMake}</h4>
+        </div>
+    )
+};
+
+const BodyComponent = () => {
+    return (
+        <div className="body">
+            <div className="resto-container">
+            <RestoComponent 
+            resName = "varalakshmi" 
+            resRating = "3.6" 
+            resDeliverytime = "30min" 
+            resMake = "Biriyani, Tandoori"
+            />
+            <RestoComponent
+            resName = "KFC - food"
+            resRating = "3.8"
+            resDeliverytime = "40min" 
+            resMake = "Fried pieces"
+            />
+            <RestoComponent
+            resName = "manohara food"
+            resRating = "5"
+            resDeliverytime = "1min" 
+            resMake = "All indian foods"
+            />
+            <RestoComponent
+             resName = "varalakshmi 2"
+             resRating = "4"
+             resDeliverytime = "20min" 
+             resMake = "Biriyani, Tandoori"
+            />
+            </div>            
+        </div>
+    )
+};
+
+
 
 const FooterComponent = () => {
-    return(<div class ="footer"><h2>my content</h2></div>)    
+    return (
+        <div className="footer"><h2>FooterComponent</h2></div>
+    )
 };
 
-const HomepageComponent = () => (
-    <div className ="container">
-        <HeaderComponent></HeaderComponent> 
-        <ContentComponent></ContentComponent>
-        <FooterComponent></FooterComponent>
-    </div>
-);
+const AppLayout = () => {
+    return (
+        <div className="container-fluid">
+            <HeaderComponent></HeaderComponent>
+            <BodyComponent></BodyComponent>
+            <FooterComponent></FooterComponent>
+        </div>
+    )
+};
 
-
-console.log(h1tag);
 
 const root = ReactDOM.createRoot(document.getElementById("root")); // it is not use mostly in realtime
 
-root.render(<HomepageComponent/>); // React js element - HTML Element
+root.render(<AppLayout></AppLayout>); // React js element - HTML Element
+
+
