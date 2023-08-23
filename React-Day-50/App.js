506bc46aee53a0d7c3609736e5825e2add928bba
vinod <vinodatla81@gmail.com>
2023-08-23 19:52:35 +0530

    added new files

--- React-Day-50/App.js
+++ React-Day-50/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 /**
- * Header component
+ * Hearder component
  * -logo
  * -Menu item
  * Body component
@@ -37,15 +37,89 @@ const HeaderComponent = () => {
     )
 };
 
+const restorentData =  {
+       id: "256893",
+       name: "McDonald's",
+       cloudinaryImageId: "ee5f8e06b300efc07c9fe3f4df40dfc4",
+       locality: "Hyderabad Kondapur LG",
+       areaName: "Forest Dept Colony",
+       costForTwo: "₹400 for two",
+       cuisines: ["Burgers", "Beverages", "Cafe", "Desserts"],
+       avgRating: 4.1,
+       feeDetails: {
+          restaurantId: "256893",
+          fees: [{
+             name: "BASE_DISTANCE",
+             fee: 5600
+          }, {
+             name: "BASE_TIME"
+          }, {
+             name: "ANCILLARY_SURGE_FEE"
+          }],
+          totalFee: 5600
+       },
+       parentId: "630",
+       avgRatingString: "4.1",
+       totalRatingsString: "10K+",
+       sla: {
+          deliveryTime: 31,
+          lastMileTravel: 6.1,
+          serviceability: "SERVICEABLE",
+          slaString: "31 mins",
+          lastMileTravelString: "6.1 km",
+          iconType: "ICON_TYPE_EMPTY"
+       },
+       availability: {
+          nextCloseTime: "2023-08-23 22:45:00",
+          opened: true
+       },
+       badges: {},
+       isOpen: true,
+       type: "F",
+       "badgesV2": {
+          entityBadges: {
+             imageBased: {},
+             textBased: {},
+             textExtendedBadges: {}
+          }
+       },
+       "aggregatedDiscountInfoV3": {
+          header: "20% OFF",
+          subHeader: "UPTO ₹50",
+          discountCalloutInfo: {
+             message: "Free Delivery",
+             logoCtx: {
+                logo: "v1655895371/free_delivery_logo_hqipbo.png"
+             }
+          }
+       },
+       orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {}
+       },
+       differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+             mediaType: "ADS_MEDIA_ENUM_IMAGE",
+             lottie: {},
+             video: {}
+          }
+       },
+       reviewsSummary: {},
+       displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+       restaurantOfferPresentationInfo: {}
+    };
+    
+
+
 const RestoComponent = (props) => {
     console.log(props);
+    // const {restoData} = props
     return (
         <div className="resto">
             <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/3632e911860bd3a90f9558fc81372b73" alt=""/>
-            <h2>{props.resName}</h2>
-            <p>{props.resRating}</p>
-            <h4>{props.resDeliverytime}</h4>
-            <h4>{props.resMake}</h4>
         </div>
     )
 };
@@ -55,12 +129,10 @@ const BodyComponent = () => {
         <div className="body">
             <div className="resto-container">
             <RestoComponent 
-            resName = "varalakshmi" 
-            resRating = "3.6" 
-            resDeliverytime = "30min" 
-            resMake = "Biriyani, Tandoori"
+            restoData = {restorentData}
+
             />
-            <RestoComponent
+            {/* <RestoComponent
             resName = "KFC - food"
             resRating = "3.8"
             resDeliverytime = "40min" 
@@ -77,7 +149,7 @@ const BodyComponent = () => {
              resRating = "4"
              resDeliverytime = "20min" 
              resMake = "Biriyani, Tandoori"
-            />
+            /> */}
             </div>            
         </div>
     )
@@ -87,9 +159,7 @@ const BodyComponent = () => {
 
 const FooterComponent = () => {
     return (
-        <div className="footer">
-            <h2>FooterComponent</h2>
-        </div>
+        <div className="footer"><h2>FooterComponent</h2></div>
     )
 };
 
---
+++ React-Day-50/README.md
@@ -0,0 +1,23 @@
+# React_app
+
+# to run the app run
+# npm install
+# npm run start
+# npm run build
+
+# to start the project do run below commands
+npm install
+npm run start
+npm run build
+
+# parcel
+Dev Build
+local server
+HMR - Hot module replacement
+cache issues
+image optimization
+manification
+code splitting
+error handling
+hosting on HTTPS
+Different bundles like Dev and PROD
